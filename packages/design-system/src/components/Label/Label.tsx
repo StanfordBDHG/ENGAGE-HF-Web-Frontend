0efeb2d9e35e0a2e09ca008e24af36f9234700c5
Arkadiusz Bachorski <60391032+arkadiuszbachorski@users.noreply.github.com>
2024-06-10 22:30:43 +0200

    Add missing compliance v2

--- packages/design-system/src/components/Label/Label.tsx
+++ packages/design-system/src/components/Label/Label.tsx
@@ -1,3 +1,10 @@
+//
+// This source file is part of the Stanford Biodesign Digital Health ENGAGE-HF open-source project
+//
+// SPDX-FileCopyrightText: 2023 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
 'use client'
 import * as LabelPrimitive from '@radix-ui/react-label'
 import { cva, type VariantProps } from 'class-variance-authority'
