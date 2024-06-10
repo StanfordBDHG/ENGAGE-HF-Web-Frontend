1591e9cb78943bc1b38755c48ed7dc3c8efac370
Arkadiusz Bachorski <60391032+arkadiuszbachorski@users.noreply.github.com>
2024-06-10 22:39:30 +0200

    Fix Card tests

--- packages/design-system/src/components/Card/Card.test.tsx
+++ packages/design-system/src/components/Card/Card.test.tsx
@@ -9,46 +9,11 @@ import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import { Card } from '.'
 
-describe('Button', () => {
-  it('renders element with button role', () => {
-    render(<Card />)
-
-    const element = screen.getByRole('button')
-    expect(element).toBeInTheDocument()
-  })
-
-  it('renders children', () => {
+describe('Card', () => {
+  it('renders element', () => {
     render(<Card>Lorem</Card>)
 
-    const element = screen.getByText('Lorem')
+    const element = screen.getByText('lorem')
     expect(element).toBeInTheDocument()
-  })
-
-  it('supports asChild property to render other type of elements with button classes', () => {
-    render(
-      <Card asChild>
-        <a href="http://example.com" />
-      </Card>,
-    )
-
-    const button = screen.queryByRole('button')
-    expect(button).not.toBeInTheDocument()
-    const link = screen.getByRole('link')
-    expect(link).toHaveAttribute('class')
-  })
-
-  it('supports isPending', () => {
-    const queryButtonPending = () => screen.queryByTestId('ButtonPending')
-    const { rerender } = render(<Card isPending={false}>Lorem</Card>)
-
-    expect(queryButtonPending()).not.toBeInTheDocument()
-
-    rerender(<Card isPending>Lorem</Card>)
-
-    expect(queryButtonPending()).toBeInTheDocument()
-    const loadingButton = screen.getByRole('button')
-    // Button should still render it's underlying content, but invisible
-    // To maintain buttons width
-    expect(loadingButton).toHaveTextContent('Lorem')
   })
 })
