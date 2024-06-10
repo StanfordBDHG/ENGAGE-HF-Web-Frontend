595c9456ef0b1d2ea5589b2d93bf6fb11db96dc8
Arkadiusz Bachorski <60391032+arkadiuszbachorski@users.noreply.github.com>
2024-06-10 22:28:48 +0200

    Add missing compliance

--- app/RegisterWorker.ts
+++ app/RegisterWorker.ts
@@ -1,5 +1,11 @@
+//
+// This source file is part of the Stanford Biodesign Digital Health ENGAGE-HF open-source project
+//
+// SPDX-FileCopyrightText: 2023 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
 'use client'
-
 import { type User } from '@firebase/auth-types'
 import { onAuthStateChanged } from 'firebase/auth'
 import { useRouter } from 'next/navigation'
--- i18n.ts
+++ i18n.ts
@@ -1,3 +1,10 @@
+//
+// This source file is part of the Stanford Biodesign Digital Health ENGAGE-HF open-source project
+//
+// SPDX-FileCopyrightText: 2023 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
 /* eslint-disable import/no-default-export */
 import { getRequestConfig } from 'next-intl/server'
 import { type AbstractIntlMessages } from 'use-intl'
