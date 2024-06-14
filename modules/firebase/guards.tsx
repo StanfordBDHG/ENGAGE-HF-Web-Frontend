a62b77e5f85231c5fe5ce7bc5c846a90f50f5668
Arkadiusz Bachorski <60391032+arkadiuszbachorski@users.noreply.github.com>
2024-06-14 21:44:55 +0200

    Fix compliance

--- modules/firebase/guards.tsx
+++ modules/firebase/guards.tsx
@@ -1,3 +1,10 @@
+//
+// This source file is part of the Stanford Biodesign Digital Health ENGAGE-HF open-source project
+//
+// SPDX-FileCopyrightText: 2023 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
 import { redirect } from 'next/navigation'
 import { getServerApp } from './serverApp'
 import { routes } from '../routes'
