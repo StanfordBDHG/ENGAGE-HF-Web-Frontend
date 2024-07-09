467cc17aee630c0cffda825701814ad31f8b560e
Arkadiusz Bachorski <60391032+arkadiuszbachorski@users.noreply.github.com>
2024-07-09 16:03:41 +0200

    Fix compliance

--- app/(dashboard)/User.tsx
+++ app/(dashboard)/User.tsx
@@ -1,5 +1,11 @@
+//
+// This source file is part of the Stanford Biodesign Digital Health ENGAGE-HF open-source project
+//
+// SPDX-FileCopyrightText: 2023 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
 'use client'
-
 import { LogOut } from 'lucide-react'
 import { auth } from '@/modules/firebase/clientApp'
 import {
