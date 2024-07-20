66270902557e75d0c85648076609a045b14a8301
Arkadiusz Bachorski <60391032+arkadiuszbachorski@users.noreply.github.com>
2024-07-20 14:58:01 +0200

    Add missing compliance

--- packages/design-system/src/components/DataTable/DataTable.utils.ts
+++ packages/design-system/src/components/DataTable/DataTable.utils.ts
@@ -1,3 +1,10 @@
+//
+// This source file is part of the Stanford Biodesign Digital Health ENGAGE-HF open-source project
+//
+// SPDX-FileCopyrightText: 2023 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
 import { type RankingInfo, rankItem } from '@tanstack/match-sorter-utils'
 import type { FilterFn } from '@tanstack/react-table'
 
--- packages/design-system/src/components/DataTable/RowDropdownMenu/RowDropdownMenu.tsx
+++ packages/design-system/src/components/DataTable/RowDropdownMenu/RowDropdownMenu.tsx
@@ -1,3 +1,10 @@
+//
+// This source file is part of the Stanford Biodesign Digital Health ENGAGE-HF open-source project
+//
+// SPDX-FileCopyrightText: 2023 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
 import { Ellipsis } from 'lucide-react'
 import { type ReactNode } from 'react'
 import { Button } from '../../Button'
