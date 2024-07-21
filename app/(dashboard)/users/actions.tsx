737d5dc021d229f4c3d3b7138596ce8251eda6a0
Arkadiusz Bachorski <60391032+arkadiuszbachorski@users.noreply.github.com>
2024-07-21 22:19:45 +0200

    Reuse routes in actions

--- app/(dashboard)/users/actions.tsx
+++ app/(dashboard)/users/actions.tsx
@@ -1,10 +1,11 @@
 'use server'
 import { revalidatePath } from 'next/cache'
 import { getAuthenticatedOnlyApp } from '@/modules/firebase/guards'
+import { routes } from '@/modules/routes'
 
 export const deleteUser = async (payload: { userId: string }) => {
   const { callables } = await getAuthenticatedOnlyApp()
   await callables.deleteUser(payload)
-  revalidatePath('/users')
+  revalidatePath(routes.users.index)
   return 'success'
 }
