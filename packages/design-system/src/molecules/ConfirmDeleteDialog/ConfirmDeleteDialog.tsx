747fd828b6f08eb695a2139d755c9d95e1abe434
Arkadiusz Bachorski <60391032+arkadiuszbachorski@users.noreply.github.com>
2024-07-22 19:17:25 +0200

    Add ConfirmDeleteDialog stories and tests

--- packages/design-system/src/molecules/ConfirmDeleteDialog/ConfirmDeleteDialog.tsx
+++ packages/design-system/src/molecules/ConfirmDeleteDialog/ConfirmDeleteDialog.tsx
@@ -14,7 +14,16 @@ import {
 } from '../../components/Dialog'
 
 interface ConfirmDeleteDialogProps extends ComponentProps<typeof Dialog> {
+  /**
+   * Name of distinctive item identifier
+   * It allows user to see what they're deleting right before confirming
+   * @example "example@example.com"
+   * */
   itemName?: ReactNode
+  /**
+   * Name of deleted entity model name
+   * @example "user"
+   * */
   entityName?: ReactNode
   onDelete: MouseEventHandler
 }
