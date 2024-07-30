e66e317b35d6576195b572cc99ae59302946e4db
Arkadiusz Bachorski <60391032+arkadiuszbachorski@users.noreply.github.com>
2024-07-30 18:48:34 +0200

    Remove emulator call

--- modules/firebase/utils.ts
+++ modules/firebase/utils.ts
@@ -131,7 +131,6 @@ export interface GetUsersInformationInput {
 }
 
 export const getCallables = (functions: Functions) => ({
-  seedEmulator: httpsCallable(functions, 'seedEmulator'),
   createInvitation: httpsCallable<
     {
       auth: {
