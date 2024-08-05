1b9428bffa3e3669655f236f3cf7ef6661a07fba
Arkadiusz Bachorski <60391032+arkadiuszbachorski@users.noreply.github.com>
2024-08-05 19:01:28 +0200

    Add medications

---
+++ app/(dashboard)/patients/[id]/Medications.tsx
@@ -0,0 +1,319 @@
+'use client'
+import { z } from 'zod'
+import { parseLocalizedText } from '@/modules/firebase/localizedText'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+  SelectGroup,
+  SelectLabel,
+} from '@/packages/design-system/src/components/Select'
+import { Field } from '@/packages/design-system/src/forms/Field'
+import { useForm } from '@/packages/design-system/src/forms/useForm'
+import { type Data } from './page'
+import { Plus, Check } from 'lucide-react'
+import { Button } from '@/packages/design-system/src/components/Button'
+import {
+  Table,
+  TableCell,
+  TableHeader,
+  TableRow,
+  TableBody,
+} from '@/packages/design-system/src/components/Table'
+
+export const quantityOptions = [
+  { label: '0.25 tbl.', value: 0.25 },
+  { label: '0.5 tbl.', value: 0.5 },
+  { label: '1 tbl.', value: 1 },
+  { label: '2 tbl.', value: 2 },
+]
+
+export const timesPerDayOptions = [
+  { label: 'once a day', value: 1 },
+  { label: 'twice a day', value: 2 },
+  { label: 'three times a day', value: 3 },
+]
+
+const formSchema = z.object({
+  medications: z.array(
+    z.object({
+      id: z.string(),
+      medication: z.string(),
+      drug: z.string(),
+      quantity: z.number().min(0),
+      frequencyPerDay: z.number().min(0),
+    }),
+  ),
+})
+
+export type MedicationsFormSchema = z.infer<typeof formSchema>
+
+interface MedicationsProps extends Data {
+  onSave: (data: MedicationsFormSchema) => Promise<void>
+  defaultValues?: MedicationsFormSchema
+}
+
+export const Medications = ({
+  medicationsTree,
+  onSave,
+  defaultValues,
+}: MedicationsProps) => {
+  const form = useForm({
+    formSchema: formSchema,
+    defaultValues,
+  })
+
+  const formValues = form.watch()
+
+  const findMedication = (id: string) => {
+    for (const medicationClass of medicationsTree) {
+      for (const medication of medicationClass.medications) {
+        if (medication.id === id) {
+          return medication
+        }
+      }
+    }
+  }
+
+  const addMedication = () =>
+    form.setValue('medications', [
+      ...formValues.medications,
+      {
+        id: `${formValues.medications.length + 1}`,
+        // `undefined` doesn't get submitted anywhere
+        medication: undefined as unknown as string,
+        drug: undefined as unknown as string,
+        quantity: 1,
+        frequencyPerDay: 1,
+      },
+    ])
+
+  const save = form.handleSubmit(async (data) => {
+    await onSave(data)
+  })
+
+  return (
+    <>
+      <header className="my-4 flex justify-end gap-2">
+        <Button size="sm" variant="secondary" onClick={addMedication}>
+          <Plus />
+          Add medication
+        </Button>
+        <Button size="sm" onClick={save}>
+          <Check />
+          Save
+        </Button>
+      </header>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableCell>Medication</TableCell>
+            <TableCell>Drug</TableCell>
+            <TableCell>Quantity</TableCell>
+            <TableCell>Frequency</TableCell>
+            <TableCell>Daily dosage</TableCell>
+            <TableCell />
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {formValues.medications.map((medicationValue, index) => {
+            const isDrugSelected = !!medicationValue.drug
+            const selectedMedication = findMedication(
+              medicationValue.medication,
+            )
+            const selectedDrug =
+              isDrugSelected ?
+                selectedMedication?.drugs.find(
+                  (drug) => drug.id === medicationValue.drug,
+                )
+              : undefined
+
+            const dailyDosage =
+              selectedDrug?.ingredients.map((drug) => ({
+                name: drug.name,
+                dosage:
+                  drug.strength *
+                  medicationValue.frequencyPerDay *
+                  medicationValue.quantity,
+              })) ?? []
+
+            const nestedKey = <T extends string>(key: T) =>
+              `medications.${index}.${key}` as const
+
+            return (
+              <TableRow key={medicationValue.id}>
+                <TableCell>
+                  <Field
+                    control={form.control}
+                    checkEmptyError
+                    name={nestedKey('medication')}
+                    render={({ field }) => (
+                      <Select
+                        onValueChange={(value) => {
+                          field.onChange(value)
+                          form.setValue(nestedKey('drug'), '')
+                        }}
+                        {...field}
+                      >
+                        <SelectTrigger>
+                          <SelectValue placeholder="Medication" />
+                        </SelectTrigger>
+                        <SelectContent>
+                          {medicationsTree.map((medicationClass) => (
+                            <SelectGroup key={medicationClass.id}>
+                              <SelectLabel>
+                                {parseLocalizedText(medicationClass.name)}
+                              </SelectLabel>
+                              {medicationClass.medications.map((medication) => (
+                                <SelectItem
+                                  value={medication.id}
+                                  key={medication.id}
+                                >
+                                  {medication.name}
+                                </SelectItem>
+                              ))}
+                            </SelectGroup>
+                          ))}
+                        </SelectContent>
+                      </Select>
+                    )}
+                  />
+                </TableCell>
+                <TableCell>
+                  <Field
+                    control={form.control}
+                    checkEmptyError
+                    name={nestedKey('drug')}
+                    render={({ field }) => (
+                      <Select
+                        disabled={!medicationValue.medication}
+                        onValueChange={(id) => {
+                          const medication = findMedication(id)
+                          if (!isDrugSelected) {
+                            // Drug selected for the first time, infer frequency and quantity
+                            form.setValue(
+                              nestedKey('frequencyPerDay'),
+                              medication?.dosage.frequencyPerDay ?? 1,
+                            )
+                            form.setValue(
+                              nestedKey('quantity'),
+                              medication?.dosage.quantity ?? 1,
+                            )
+                          }
+                          field.onChange(id)
+                        }}
+                        {...field}
+                      >
+                        <SelectTrigger>
+                          <SelectValue placeholder="Drug" />
+                        </SelectTrigger>
+                        <SelectContent>
+                          {selectedMedication?.drugs.map((drug) => (
+                            <SelectItem
+                              value={drug.id}
+                              key={drug.id}
+                              itemText={`${drug.name} - ${drug.ingredients
+                                .map(
+                                  (ingredient) =>
+                                    `${ingredient.strength}${ingredient.unit}`,
+                                )
+                                .join(', ')}`}
+                            >
+                              <div className="flex flex-col">
+                                <b>{drug.name}</b>
+                                {drug.ingredients.map((ingredient) => (
+                                  <p key={ingredient.name}>
+                                    {ingredient.name} - {ingredient.strength}
+                                    {ingredient.unit}
+                                  </p>
+                                ))}
+                              </div>
+                            </SelectItem>
+                          ))}
+                        </SelectContent>
+                      </Select>
+                    )}
+                  />
+                </TableCell>
+                <TableCell>
+                  <Field
+                    control={form.control}
+                    checkEmptyError
+                    name={nestedKey('quantity')}
+                    render={({ field }) => (
+                      <Select
+                        {...field}
+                        disabled={!isDrugSelected}
+                        onValueChange={(value) => field.onChange(Number(value))}
+                        value={String(field.value)}
+                      >
+                        <SelectTrigger>
+                          <SelectValue placeholder="Drug" />
+                        </SelectTrigger>
+                        <SelectContent>
+                          {quantityOptions.map((option) => (
+                            <SelectItem
+                              key={option.value}
+                              value={String(option.value)}
+                            >
+                              {option.label}
+                            </SelectItem>
+                          ))}
+                        </SelectContent>
+                      </Select>
+                    )}
+                  />
+                </TableCell>
+                <TableCell>
+                  <Field
+                    control={form.control}
+                    checkEmptyError
+                    name={nestedKey('frequencyPerDay')}
+                    render={({ field }) => (
+                      <Select
+                        {...field}
+                        disabled={!isDrugSelected}
+                        onValueChange={(value) => field.onChange(Number(value))}
+                        value={String(field.value)}
+                      >
+                        <SelectTrigger>
+                          <SelectValue placeholder="Frequency" />
+                        </SelectTrigger>
+                        <SelectContent>
+                          {timesPerDayOptions.map((option) => (
+                            <SelectItem
+                              key={option.value}
+                              value={String(option.value)}
+                            >
+                              {option.label}
+                            </SelectItem>
+                          ))}
+                        </SelectContent>
+                      </Select>
+                    )}
+                  />
+                </TableCell>
+                <TableCell>
+                  {dailyDosage.length === 0 ?
+                    '-'
+                  : dailyDosage.length === 1 ?
+                    `${dailyDosage.at(0)?.dosage}mg`
+                  : <div>
+                      {dailyDosage.map((dosage) => (
+                        <div key={dosage.name}>
+                          {dosage.name} - {dosage.dosage}mg
+                        </div>
+                      ))}
+                    </div>
+                  }
+                </TableCell>
+              </TableRow>
+            )
+          })}
+        </TableBody>
+      </Table>
+    </>
+  )
+}
--- app/(dashboard)/patients/[id]/page.tsx
+++ app/(dashboard)/patients/[id]/page.tsx
@@ -5,7 +5,7 @@
 //
 // SPDX-License-Identifier: MIT
 //
-import { updateDoc } from '@firebase/firestore'
+import { setDoc, updateDoc } from '@firebase/firestore'
 import { Contact } from 'lucide-react'
 import { revalidatePath } from 'next/cache'
 import { notFound } from 'next/navigation'
@@ -16,11 +16,125 @@ import {
 import { getFormProps } from '@/app/(dashboard)/patients/utils'
 import { getAuthenticatedOnlyApp } from '@/modules/firebase/guards'
 import { mapAuthData } from '@/modules/firebase/user'
-import { getDocDataOrThrow, UserType } from '@/modules/firebase/utils'
+import {
+  getDocDataOrThrow,
+  getDocsData,
+  UserType,
+} from '@/modules/firebase/utils'
 import { routes } from '@/modules/routes'
 import { getUserName } from '@/packages/design-system/src/modules/auth/user'
 import { PageTitle } from '@/packages/design-system/src/molecules/DashboardLayout'
+import { Medications, MedicationsFormSchema } from './Medications'
 import { DashboardLayout } from '../../DashboardLayout'
+import { groupBy } from 'es-toolkit'
+import {
+  TabsList,
+  Tabs,
+  TabsContent,
+  TabsTrigger,
+} from '@/packages/design-system/src/components/Tabs'
+
+// TODO: Cache this
+const getMedicationsData = async () => {
+  const { refs } = await getAuthenticatedOnlyApp()
+  const medicationClasses = await getDocsData(refs.medicationClasses())
+  const medicationsDocs = await getDocsData(refs.medications())
+
+  const prefix = 'medicationClasses'
+
+  const getMedications = medicationsDocs.map(async (doc) => {
+    const medicationClassExtension = doc.extension?.find((extension) =>
+      extension.valueReference?.reference?.startsWith(prefix),
+    )
+    const medicationClassId =
+      medicationClassExtension?.valueReference?.reference?.slice(
+        prefix.length + 1,
+      )
+
+    const drugsDocs = await getDocsData(refs.drugs(doc.id))
+    const dosageInstruction = doc.extension
+      ?.find(
+        (extension) =>
+          extension.valueMedicationRequest &&
+          extension.url.endsWith('/targetDailyDose'),
+      )
+      ?.valueMedicationRequest?.dosageInstruction?.at(0)
+
+    return {
+      id: doc.id,
+      name: doc.code?.coding?.at(0)?.display ?? '',
+      medicationClassId,
+      dosage: {
+        frequencyPerDay: dosageInstruction?.timing?.repeat?.period ?? 1,
+        quantity:
+          dosageInstruction?.doseAndRate?.at(0)?.doseQuantity?.value ?? 1,
+      },
+      drugs: drugsDocs
+        .map((drug) => ({
+          id: drug.id,
+          medicationId: doc.id,
+          medicationClassId,
+          name: drug.code?.coding?.at(0)?.display ?? '',
+          ingredients:
+            drug.ingredient?.map((ingredient) => {
+              const name =
+                ingredient.itemCodeableConcept?.coding?.at(0)?.display ?? ''
+              const unit = ingredient.strength?.numerator?.unit ?? ''
+              const strength =
+                (ingredient.strength?.numerator?.value ?? 1) /
+                (ingredient.strength?.denominator?.value ?? 1)
+              return {
+                name,
+                strength,
+                unit,
+              }
+            }) ?? [],
+        }))
+        .sort((a, b) => {
+          const name = a.name.localeCompare(b.name)
+          return name === 0 ?
+              (a.ingredients.at(0)?.strength ?? 0) -
+                (b.ingredients.at(0)?.strength ?? 0)
+            : name
+        }),
+    }
+  })
+
+  const formattedMedications = await Promise.all(getMedications)
+  const medicationsByClass = groupBy(
+    formattedMedications,
+    (medication) => medication.medicationClassId ?? '',
+  )
+
+  const medicationsTree = medicationClasses.map((medicationClass) => ({
+    id: medicationClass.id,
+    name: medicationClass.name,
+    medications: medicationsByClass[medicationClass.id] ?? [],
+  }))
+
+  return { medicationsTree }
+}
+
+const getUserMedications = async (userId: string) => {
+  const { refs } = await getAuthenticatedOnlyApp()
+  const medicationRequests = await getDocsData(refs.medicationRequests(userId))
+  return medicationRequests.map((request) => {
+    // TODO: Implement stronger mechanism
+    const reference = request.medicationReference?.reference?.split('/')
+    return {
+      id: request.id,
+      medication: reference?.at(1) ?? '',
+      drug: reference?.at(3) ?? '',
+      frequencyPerDay:
+        request?.dosageInstruction?.at(0)?.timing?.repeat?.frequency ?? 1,
+      quantity:
+        request?.dosageInstruction?.at(0)?.doseAndRate?.at(0)?.doseQuantity
+          ?.value ?? 1,
+    }
+  })
+}
+
+export type Data = Awaited<ReturnType<typeof getMedicationsData>>
 
 interface PatientPageProps {
   params: { id: string }
@@ -62,6 +176,41 @@ const PatientPage = async ({ params }: PatientPageProps) => {
     revalidatePath(routes.users.index)
   }
 
+  const saveMedications = async (form: MedicationsFormSchema) => {
+    'use server'
+    const { docRefs } = await getAuthenticatedOnlyApp()
+    // TODO: Handle removing previous data, probably a transaction
+    const promises = form.medications.map(async (medication) => {
+      await setDoc(docRefs.medicationRequest(userId, medication.id), {
+        medicationReference: {
+          reference: `medications/${medication.medication}/drugs/${medication.drug}`,
+        },
+        dosageInstruction: [
+          {
+            timing: {
+              repeat: {
+                frequency: medication.frequencyPerDay,
+                period: 1,
+                periodUnit: 'd',
+              },
+            },
+            doseAndRate: [
+              {
+                doseQuantity: {
+                  code: '{tbl}',
+                  system: 'http://unitsofmeasure.org',
+                  unit: 'tbl.',
+                  value: medication.quantity,
+                },
+              },
+            ],
+          },
+        ],
+      })
+    })
+    await Promise.all(promises)
+  }
+
   return (
     <DashboardLayout
       title={
@@ -72,12 +221,33 @@ const PatientPage = async ({ params }: PatientPageProps) => {
         />
       }
     >
-      <PatientForm
-        user={user}
-        userInfo={authUser}
-        onSubmit={updatePatient}
-        {...await getFormProps()}
-      />
+      <Tabs defaultValue="medication">
+        <TabsList className="w-full">
+          <TabsTrigger value="information" className="grow">
+            Information
+          </TabsTrigger>
+          <TabsTrigger value="medication" className="grow">
+            Medication
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="information" className="mt-6">
+          <PatientForm
+            user={user}
+            userInfo={authUser}
+            onSubmit={updatePatient}
+            {...await getFormProps()}
+          />
+        </TabsContent>
+        <TabsContent value="medication">
+          <Medications
+            {...await getMedicationsData()}
+            onSave={saveMedications}
+            defaultValues={{
+              medications: await getUserMedications(userId),
+            }}
+          />
+        </TabsContent>
+      </Tabs>
     </DashboardLayout>
   )
 }
