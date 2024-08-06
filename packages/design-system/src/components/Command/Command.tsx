4d6858c2a76949eafac2cce943e0716f481646bd
Arkadiusz Bachorski <60391032+arkadiuszbachorski@users.noreply.github.com>
2024-08-06 17:25:08 +0200

    Fix compliance

--- packages/design-system/src/components/Command/Command.tsx
+++ packages/design-system/src/components/Command/Command.tsx
@@ -1,5 +1,11 @@
+//
+// This source file is part of the Stanford Biodesign Digital Health ENGAGE-HF open-source project
+//
+// SPDX-FileCopyrightText: 2023 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
 'use client'
-
 import { type DialogProps } from '@radix-ui/react-dialog'
 import { Command as CommandPrimitive } from 'cmdk'
 import { Search } from 'lucide-react'
--- packages/design-system/src/components/Tabs/Tabs.tsx
+++ packages/design-system/src/components/Tabs/Tabs.tsx
@@ -1,5 +1,11 @@
+//
+// This source file is part of the Stanford Biodesign Digital Health ENGAGE-HF open-source project
+//
+// SPDX-FileCopyrightText: 2023 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
 'use client'
-
 import * as TabsPrimitive from '@radix-ui/react-tabs'
 import {
   type ComponentPropsWithoutRef,
