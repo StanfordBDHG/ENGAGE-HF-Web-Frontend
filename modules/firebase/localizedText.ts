dde0661a5c2ecf704bc629529123c27799dd84d2
Arkadiusz Bachorski <60391032+arkadiuszbachorski@users.noreply.github.com>
2024-09-22 21:22:00 +0200

    Add parseNilLocalizedText helper

--- modules/firebase/localizedText.ts
+++ modules/firebase/localizedText.ts
@@ -7,8 +7,13 @@
 //
 import { isObject } from 'lodash'
 import { type LocalizedText } from '@/modules/firebase/models'
+import { Nil } from '@/packages/design-system/src/utils/misc'
+import { isNil } from 'es-toolkit'
 
 const locale = 'en'
 
 export const parseLocalizedText = (text: LocalizedText) =>
   isObject(text) ? text[locale] : text
+
+export const parseNilLocalizedText = (text: Nil<LocalizedText>) =>
+  isNil(text) ? null : parseLocalizedText(text)
