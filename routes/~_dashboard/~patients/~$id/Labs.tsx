46a2d66647c4673024b0a30cbb4e448d12ceed2f
Arkadiusz Bachorski <60391032+arkadiuszbachorski@users.noreply.github.com>
2024-09-27 13:39:59 +0200

    temp

--- routes/~_dashboard/~patients/~$id/Labs.tsx
+++ routes/~_dashboard/~patients/~$id/Labs.tsx
@@ -5,23 +5,33 @@
 //
 // SPDX-License-Identifier: MIT
 //
+import { deleteDoc } from '@firebase/firestore'
 import { useRouter } from '@tanstack/react-router'
 import { createColumnHelper } from '@tanstack/table-core'
-import { Plus } from 'lucide-react'
-import { useMemo } from 'react'
+import { Pencil, Plus, Trash } from 'lucide-react'
+import { useMemo, useState } from 'react'
+import { docRefs } from '@/modules/firebase/app'
 import { Button } from '@/packages/design-system/src/components/Button'
 import {
   DataTable,
   dateColumn,
+  RowDropdownMenu,
 } from '@/packages/design-system/src/components/DataTable'
+import { DropdownMenuItem } from '@/packages/design-system/src/components/DropdownMenu'
+import { ConfirmDeleteDialog } from '@/packages/design-system/src/molecules/ConfirmDeleteDialog'
 import { useOpenState } from '@/packages/design-system/src/utils/useOpenState'
-import { createObservation } from '@/routes/~_dashboard/~patients/actions'
+import {
+  createObservation,
+  updateObservation,
+} from '@/routes/~_dashboard/~patients/actions'
 import type {
   LabsData,
   Observation,
 } from '@/routes/~_dashboard/~patients/utils'
-import { LabFormDialog } from '@/routes/~_dashboard/~patients/~$id/LabForm'
-import { LabMenu } from '@/routes/~_dashboard/~patients/~$id/LabMenu'
+import {
+  LabFormDialog,
+  type LabFormSchema,
+} from '@/routes/~_dashboard/~patients/~$id/LabForm'
 
 interface LabsProps extends LabsData {}
 
@@ -30,6 +40,48 @@ const columnHelper = createColumnHelper<Observation>()
 export const Labs = ({ observations, userId, resourceType }: LabsProps) => {
   const router = useRouter()
   const createDialog = useOpenState()
+
+  const [observationToDelete, setObservationToDelete] = useState<Observation>()
+  const closeDelete = () => setObservationToDelete(undefined)
+
+  const [observationToEdit, setObservationToEdit] = useState<Observation>()
+  const closeEdit = () => setObservationToEdit(undefined)
+
+  const handleDelete = async () => {
+    if (!observationToDelete) return
+    await deleteDoc(
+      docRefs.userObservation({
+        userId,
+        resourceType,
+        observationId: observationToDelete.id,
+        observationType: observationToDelete.type,
+      }),
+    )
+    await router.invalidate()
+    closeDelete()
+  }
+
+  const handleEdit = async (data: LabFormSchema) => {
+    if (!observationToEdit) return
+    await updateObservation({
+      userId,
+      resourceType,
+      observationId: observationToEdit.id,
+      ...data,
+    })
+    await router.invalidate()
+    closeEdit()
+  }
+
+  const handleCreate = async (data: LabFormSchema) => {
+    await createObservation({
+      userId,
+      resourceType,
+      ...data,
+    })
+    await router.invalidate()
+    createDialog.close()
+  }
 
   const columns = useMemo(
     () => [
@@ -49,32 +101,48 @@ export const Labs = ({ observations, userId, resourceType }: LabsProps) => {
       }),
       columnHelper.display({
         id: 'actions',
-        cell: (props) => (
-          <LabMenu
-            observation={props.row.original}
-            userId={userId}
-            resourceType={resourceType}
-          />
-        ),
+        cell: (props) => {
+          const observation = props.row.original
+          return (
+            <RowDropdownMenu>
+              <DropdownMenuItem
+                onClick={() => setObservationToEdit(observation)}
+              >
+                <Pencil />
+                Edit
+              </DropdownMenuItem>
+              <DropdownMenuItem
+                onClick={() => setObservationToDelete(observation)}
+              >
+                <Trash />
+                Delete
+              </DropdownMenuItem>
+            </RowDropdownMenu>
+          )
+        },
       }),
     ],
-    [resourceType, userId],
+    [],
   )
 
   return (
     <>
       <LabFormDialog
-        onSubmit={async (data) => {
-          await createObservation({
-            userId,
-            resourceType,
-            ...data,
-          })
-          await router.invalidate()
-          createDialog.close()
-        }}
+        onSubmit={handleEdit}
+        open={!!observationToEdit}
+        onOpenChange={closeEdit}
+        observation={observationToEdit}
+      />
+      <LabFormDialog
+        onSubmit={handleCreate}
         open={createDialog.isOpen}
         onOpenChange={createDialog.setIsOpen}
+      />
+      <ConfirmDeleteDialog
+        open={!!observationToDelete}
+        onOpenChange={closeDelete}
+        entityName="lab"
+        onDelete={handleDelete}
       />
       <DataTable
         columns={columns}
