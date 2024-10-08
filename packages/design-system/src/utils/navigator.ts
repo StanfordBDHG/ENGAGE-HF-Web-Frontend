4c6d224094875fc7aa9a4a7c6e7dd764980dc501
Arkadiusz Bachorski <60391032+arkadiuszbachorski@users.noreply.github.com>
2024-10-08 13:15:26 +0200

    Fix compliance

--- packages/design-system/src/utils/navigator.ts
+++ packages/design-system/src/utils/navigator.ts
@@ -1,3 +1,10 @@
+//
+// This source file is part of the Stanford Biodesign Digital Health ENGAGE-HF open-source project
+//
+// SPDX-FileCopyrightText: 2023 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
 /* eslint-disable @typescript-eslint/no-unnecessary-condition */
 /**
  * Gets user locale, providing en-US as fallback
