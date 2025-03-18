4ad15e6b15dc36e453d216008a36173449c2e348
Arkadiusz Bachorski <60391032+arkadiuszbachorski@users.noreply.github.com>
2025-03-18 15:38:04 +0100

    Handle share report logic

--- routes/~patients/~$userId/~healthSummary/~$shareCodeId/~index.tsx
+++ routes/~patients/~$userId/~healthSummary/~$shareCodeId/~index.tsx
@@ -6,9 +6,66 @@
 // SPDX-License-Identifier: MIT
 //
 
+import { ErrorState } from "@stanfordspezi/spezi-web-design-system/components/ErrorState";
+import { InputOTP } from "@stanfordspezi/spezi-web-design-system/components/InputOTP";
+import { Spinner } from "@stanfordspezi/spezi-web-design-system/components/Spinner";
+import { base64ToBlob } from "@stanfordspezi/spezi-web-design-system/utils/file";
+import { useMutation } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
+import { Helmet } from "react-helmet";
+import { AsideEngageLayout } from "@/components/AsideEngageLayout";
+import { callables } from "@/modules/firebase/app";
 
-const ViewHealthSummary = () => <>....</>;
+const ViewHealthSummary = () => {
+  const { userId, shareCodeId } = Route.useParams();
+
+  const exportHealthSummaryMutation = useMutation({
+    mutationFn: callables.exportHealthSummary,
+    onSuccess: ({ data }) => {
+      const blob = base64ToBlob(data.content, "application/pdf");
+      const url = URL.createObjectURL(blob);
+      window.location.href = url;
+    },
+  });
+
+  return (
+    <AsideEngageLayout>
+      <Helmet>
+        <title>View Health Summary Report</title>
+      </Helmet>
+      <div className="relative flex flex-col items-center">
+        {exportHealthSummaryMutation.isPending && (
+          <div className="flex-center absolute left-0 top-0 z-10 size-full bg-surface/60 animate-in fade-in zoom-in">
+            <Spinner />
+          </div>
+        )}
+        <h1 className="mb-1 text-center text-2xl font-bold">
+          View Health Summary Report
+        </h1>
+        <p className="mb-6 text-center text-sm text-muted-foreground">
+          Enter one-time code to view the report
+        </p>
+        <InputOTP
+          maxLength={8}
+          onComplete={(shareCode: string) =>
+            exportHealthSummaryMutation.mutate({
+              userId,
+              shareCodeId,
+              shareCode,
+            })
+          }
+        />
+        {exportHealthSummaryMutation.isError && (
+          <ErrorState className="mt-8">
+            Unable to view report. Your access code may be incorrect or expired.
+            <br />
+            Please try again.
+          </ErrorState>
+        )}
+      </div>
+    </AsideEngageLayout>
+  );
+};
 
 export const Route = createFileRoute(
   "/patients/$userId/healthSummary/$shareCodeId/",
