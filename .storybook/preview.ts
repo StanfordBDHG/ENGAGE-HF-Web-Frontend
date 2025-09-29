73183a6ce21e8d42aca9247c54d2136c57807cb0
Arkadiusz Bachorski <60391032+arkadiuszbachorski@users.noreply.github.com>
2025-09-29 21:59:49 +0200

    Add missing compliance

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,3 +1,11 @@
+//
+// This source file is part of the Stanford Biodesign Digital Health ENGAGE-HF open-source project
+//
+// SPDX-FileCopyrightText: 2023 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
+
 import type { Preview } from "@storybook/react-vite";
 
 const preview: Preview = {
